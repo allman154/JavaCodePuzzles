e40f2617ef366701e0bcfca350dcdfcd061f42a7
allman154 <esper154@gmail.com>
2025-10-03 11:32:32 -0400

    First Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Create and
         Students studentList = new Students();
         studentList.addStudentToList(10);
         studentList.addStudentToList(20);
@@ -8,14 +9,19 @@ public class App {
         studentList.addStudentToList(50);
         studentList.addStudentToList(60);
 
+        // display basic list
         studentList.displayStudentList();
 
+        // remove and item
         studentList.removeItem(30);
 
+        // display updated list
         studentList.displayStudentList();
 
+        // invert the pointers
         studentList.reverseList();
 
+        // display altered list
         studentList.displayStudentList();
     }
 }
